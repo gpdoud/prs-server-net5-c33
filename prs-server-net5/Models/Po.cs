09d9c395258f8634e6093e3806a693000ed8c7da
gpdoud <gpdoud@gmail.com>
2021-11-18 23:15:54 -0500

    Added Po and Poline

---
+++ prs-server-net5/Models/Po.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace prs_server_net5.Models {
+
+    public class Po {
+
+        public Vendor Vendor { get; set; }
+        public User User { get; set; }
+        public List<Poline> Polines { get; set; }
+
+        public string Description { get; set; }
+        public decimal Total { get; set; }
+
+        public Po() {}
+    }
+}
+
---
+++ prs-server-net5/Models/Poline.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace prs_server_net5.Models {
+
+    public class Poline {
+
+        public string PartNbr { get; set; }
+        public string Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+
+        public Poline() {}
+    }
+}
+
